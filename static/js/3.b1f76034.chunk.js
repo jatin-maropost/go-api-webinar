0f35c4d2849da9b69deb7d8d6f37ee222db60208
jatin-maropost <jatin.kamboj@maropost.com>
2021-07-24 00:58:32 +0530

    Updates

---
+++ static/js/3.b1f76034.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpbook-store"]=this["webpackJsonpbook-store"]||[]).push([[3],{28:function(e,o,t){"use strict";t.r(o);t(0);var s=t(6);o.default=function(e){return Object(s.jsx)("h1",{children:"Home here"})}}}]);
+//# sourceMappingURL=3.b1f76034.chunk.js.map
